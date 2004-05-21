989f6a21b9f3e0dd63683db10d6fd60b99c50d76
fitz <fitz@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2004-05-21 21:23:20 +0000

    Implement change_path repository function.
    * cvs2svn.py (SVNRepositoryMirror.change_path): New function. 
    
      (SVNRepositoryMirror.commit): Call change_path now.

--- branches/may-04-redesign/cvs2svn.py
+++ branches/may-04-redesign/cvs2svn.py
@@ -3877,6 +3877,41 @@ class SVNRepositoryMirror:
     root_key = self.revs_db[self._youngest_key()]
     self._stabilize_directory(root_key)
 
+  ###TODO add_path and change_path share too much code. Share!
+  def change_path(self, cvs_rev):
+    """Register a change in self.youngest for the CVS_REV's svn_path
+    in the repository mirror."""
+    parent_node_key, parent_node_contents = self._get_youngest_root_node()
+
+    path_so_far = ""
+    # Walk up the path, one node at a time.
+    components = cvs_rev.svn_path.split('/')
+    for component in components:
+      path_so_far = os.path.join(path_so_far, component)
+      print "ZOO:", path_so_far
+
+      this_entry_key = parent_node_contents[component]
+      this_entry_contents = self.nodes_db[this_entry_key]
+      mutable = this_entry_contents.get(self.mutable_flag)
+      if not mutable:
+        print "ZOO: NEW NODE IN rev", self.youngest
+        # Create a new, mutable node.
+        this_entry_key = self._make_node(this_entry_contents)
+        # Replace the old node key in the parent node
+        parent_node_contents[component] = this_entry_key
+        # Update the parent node in the db
+        self.nodes_db[parent_node_key] = parent_node_contents
+      else:
+        print "ZOO: OLD NODE IN rev", self.youngest
+
+      ###TODO Shouldn't something like this be in delete_path?
+      #elif mutable == 2:
+      #  in_pruneable_subtree = 1
+      parent_node_key = this_entry_key
+      parent_node_contents = this_entry_contents
+
+
+
 
   def add_path(self, cvs_rev):
     """Add the CVS_REV's svn_path to the repository mirror."""
@@ -3956,13 +3991,14 @@ class SVNRepositoryMirror:
     if svn_commit.symbolic_name:
       ## This will entail nothing more than copying and deleting.
       pass # This is a branch/tag create/fill/finish
-      
+
     else: # This will actually commit CVSRevisions
       for cvs_rev in svn_commit.cvs_revs:
         if cvs_rev.op == OP_ADD:
           self.add_path(cvs_rev)
           self.delegate.add_path(cvs_rev)
         elif cvs_rev.op == OP_CHANGE:
+          self.change_path(cvs_rev)
           self.delegate.change_path(cvs_rev)
         else: # Must be a delete
           self.delegate.delete_path(cvs_rev)
