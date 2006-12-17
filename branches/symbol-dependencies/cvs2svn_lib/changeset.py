14a13f61511f7c4c8c2a3e0ef7b5db0b6ad4f1c5
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2006-12-17 12:09:57 +0000

    Add a new OrderedChangeset class.
    * cvs2svn_lib/changeset.py (OrderedChangeset): New class.
    
    * cvs2svn_lib/changeset_database.py (ChangesetDatabase.__init__): Add
      OrderedChangeset to primer.

--- branches/symbol-dependencies/cvs2svn_lib/changeset.py
+++ branches/symbol-dependencies/cvs2svn_lib/changeset.py
@@ -81,6 +81,60 @@ class RevisionChangeset(Changeset):
     return 'RevisionChangeset<%x>' % (self.id,)
 
 
+class OrderedChangeset(Changeset):
+  """A Changeset of CVSRevisions whose preliminary order is known.
+
+  The first changeset ordering involves only RevisionChangesets, and
+  results in a full ordering of RevisionChangesets (i.e., a linear
+  chain of dependencies with the order consistent with the
+  dependencies).  These OrderedChangesets form the skeleton for the
+  full topological sort that includes SymbolChangesets as well."""
+
+  def __init__(self, id, cvs_item_ids, prev_id, next_id):
+    Changeset.__init__(self, id, cvs_item_ids)
+
+    # The changeset id of the previous OrderedChangeset, or None if
+    # this is the first OrderedChangeset:
+    self.prev_id = prev_id
+
+    # The changeset id of the next OrderedChangeset, or None if this
+    # is the last OrderedChangeset:
+    self.next_id = next_id
+
+  def create_graph_node(self):
+    time_range = TimeRange()
+
+    pred_ids = set()
+    succ_ids = set()
+
+    if self.prev_id is not None:
+      pred_ids.add(self.prev_id)
+
+    if self.next_id is not None:
+      succ_ids.add(self.next_id)
+
+    for cvs_item in self.get_cvs_items():
+      time_range.add(cvs_item.timestamp)
+
+      for pred_id in cvs_item.get_symbol_pred_ids():
+        pred_ids.add(Ctx()._cvs_item_to_changeset_id[pred_id])
+
+      for succ_id in cvs_item.get_symbol_succ_ids():
+        succ_ids.add(Ctx()._cvs_item_to_changeset_id[succ_id])
+
+    return ChangesetGraphNode(self.id, time_range, pred_ids, succ_ids)
+
+  def __getstate__(self):
+    return Changeset.__getstate__(self) + (self.prev_id, self.next_id,)
+
+  def __setstate__(self, state):
+    Changeset.__setstate__(self, state[:-2])
+    (self.prev_id, self.next_id,) = state[-2:]
+
+  def __str__(self):
+    return 'OrderedChangeset<%x>' % (self.id,)
+
+
 class SymbolChangeset(Changeset):
   """A Changeset consisting of CVSSymbols."""
 
